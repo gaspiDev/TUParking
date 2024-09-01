f13bf1d470abe0962a3bac245159797a865b8153
gaspi <gaspi>
2024-08-31 23:16:42 -0300

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { ParkingStateComponent } from './pages/parking-state/parking-state.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "signup",
+    component: SignupComponent
+  },
+  {
+    path: "parking-state",
+    component: ParkingStateComponent
+  },
+  {
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full"
+  },
+  {
+    path: "not-found",
+    component: NotFoundComponent
+  },
+  {
+    path: "**",
+    redirectTo: "not-found",
+    pathMatch: "full"
+  },
+];
---
+++ src/app/pages/parking-state/parking-state.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-parking-state',
+  standalone: true,
+  imports: [],
+  templateUrl: './parking-state.component.html',
+  styleUrl: './parking-state.component.scss'
+})
+export class ParkingStateComponent {
+
+}
