3b0a186028a429dd7f43bde2deae55640aa34a69
gaspi <gaspi>
2024-09-05 00:29:03 -0300

    progress

---
+++ src/app/Interfaces/parking.ts
@@ -0,0 +1,5 @@
+export interface Parking{
+  number: number,
+  available: boolean,
+  entry: string,
+}
--- src/app/pages/parking-state/parking-state.component.ts
+++ src/app/pages/parking-state/parking-state.component.ts
@@ -1,13 +1,56 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Parking } from '../../Interfaces/parking';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-parking-state',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './parking-state.component.html',
   styleUrl: './parking-state.component.scss'
 })
 export class ParkingStateComponent {
 
+  title: string = 'TUParking';
+ 
+  tableHeader = {
+    c1: 'Spot Number',
+    c2: 'Availability',
+    c3: 'Ingress Time',
+    c4: 'Actions'
+  }
+
+  parking: Parking[] = []
+
+  lastNumber = this.parking[this.parking.length-1]?.number || 0;
+  addNew(){
+    this.parking.push(
+      {
+        number: this.lastNumber + 1,
+        available: true,
+        entry: '-'
+      }
+    )
+  this.lastNumber++;
+  }
+
+  deleteAll(){
+    this.parking = []
+    this.lastNumber = 0
+  }
+
+  deleteThatSpot(index: number){
+    this.parking.splice(index, 1)
+  }
+
+  occupyParkingState(index: number){
+    this.parking[index].available = false
+  }
+
+  setFreeParkingState(index: number){
+    this.parking[index].available = true
+  }
 }
+
